37ab9e3371c91bab0434df68a265316a652fd969
LiLei <tobiaslee@ralee.cc>
2018-08-30 11:49:40 +0800

    rewrite code

--- models/attn_bi_lstm.py
+++ models/attn_bi_lstm.py
@@ -1,117 +1,154 @@
-from tensorflow.contrib.rnn import BasicLSTMCell
 from tensorflow.python.ops.rnn import bidirectional_dynamic_rnn as bi_rnn
-import time
+from tensorflow.contrib.rnn import BasicLSTMCell
 from utils.prepare_data import *
-
-# Hyperparameter
-MAX_DOCUMENT_LENGTH = 25
-EMBEDDING_SIZE = 128
-HIDDEN_SIZE = 64
-ATTENTION_SIZE = 64
-lr = 1e-3
-BATCH_SIZE = 256
-KEEP_PROB = 0.5
-LAMBDA = 0.0001
-
-MAX_LABEL = 15
-epochs = 10
-
-# load data
-x_train, y_train = load_data("../dbpedia_data/dbpedia_csv/train.csv")
-x_test, y_test = load_data("../dbpedia_data/dbpedia_csv/test.csv")
-
-# data preprocessing
-x_train, x_test, vocab_size = \
-    data_preprocessing_v2(x_train, x_test, MAX_DOCUMENT_LENGTH)
-print(vocab_size)
-
-# split dataset to test and dev
-x_test, x_dev, y_test, y_dev, dev_size, test_size = \
-    split_dataset(x_test, y_test, 0.1)
-print("Validation size: ", dev_size)
-
-graph = tf.Graph()
-with graph.as_default():
-
-    batch_x = tf.placeholder(tf.int32, [None, MAX_DOCUMENT_LENGTH])
-    batch_y = tf.placeholder(tf.float32, [None, MAX_LABEL])
-    keep_prob = tf.placeholder(tf.float32)
-
-    embeddings_var = tf.Variable(tf.random_uniform([vocab_size, EMBEDDING_SIZE], -1.0, 1.0), trainable=True)
-    batch_embedded = tf.nn.embedding_lookup(embeddings_var, batch_x)
-    W = tf.Variable(tf.random_normal([HIDDEN_SIZE], stddev=0.1))
-    # print(batch_embedded.shape)  # (?, 256, 100)
-
-    rnn_outputs, _ = bi_rnn(BasicLSTMCell(HIDDEN_SIZE), BasicLSTMCell(HIDDEN_SIZE),
-                            inputs=batch_embedded, dtype=tf.float32)
-    # Attention
-    fw_outputs = rnn_outputs[0]
-    # print(fw_outputs.shape)
-    bw_outputs = rnn_outputs[1]
-    H = fw_outputs + bw_outputs  # (batch_size, seq_len, HIDDEN_SIZE)
-    M = tf.tanh(H) # M = tanh(H)  (batch_size, seq_len, HIDDEN_SIZE)
-    # print(M.shape)
-    # alpha (bs * sl, 1)
-    alpha = tf.nn.softmax(tf.matmul(tf.reshape(M, [-1, HIDDEN_SIZE]), tf.reshape(W, [-1, 1])))
-    r = tf.matmul(tf.transpose(H, [0, 2, 1]), tf.reshape(alpha, [-1, MAX_DOCUMENT_LENGTH, 1])) # supposed to be (batch_size * HIDDEN_SIZE, 1)
-    # print(r.shape)
-    r = tf.squeeze(r)
-    h_star = tf.tanh(r) # (batch , HIDDEN_SIZE
-    # attention_output, alphas = attention(rnn_outputs, ATTENTION_SIZE, return_alphas=True)
+import time
 
 
-    drop = tf.nn.dropout(h_star, keep_prob)
-    # shape = drop.get_shape()
-    # print(shape)
+class ABLSTM(object):
+    def __init__(self, config):
+        self.max_len = config["max_len"]
+        self.hidden_size = config["hidden_size"]
+        self.vocab_size = config["vocab_size"]
+        self.embedding_size = config["embedding_size"]
+        self.n_class = config["n_class"]
+        self.learning_rate = config["learning_rate"]
 
-    # Fully connected layer（dense layer)
-    W = tf.Variable(tf.truncated_normal([HIDDEN_SIZE, MAX_LABEL], stddev=0.1))
-    b = tf.Variable(tf.constant(0., shape=[MAX_LABEL]))
-    y_hat = tf.nn.xw_plus_b(drop, W, b)
-    # print(y_hat.shape)
+        # placeholder
+        self.x = tf.placeholder(tf.int32, [None, self.max_len])
+        self.label = tf.placeholder(tf.int32, [None])
+        self.keep_prob = tf.placeholder(tf.float32)
 
-    # y_hat = tf.squeeze(y_hat)
+    def build_graph(self):
+        print("building graph")
+        # Word embedding
+        embeddings_var = tf.Variable(tf.random_uniform([self.vocab_size, self.embedding_size], -1.0, 1.0),
+                                     trainable=True)
+        batch_embedded = tf.nn.embedding_lookup(embeddings_var, self.x)
 
-    loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=y_hat, labels=batch_y))
-    optimizer = tf.train.AdamOptimizer(learning_rate=lr).minimize(loss)
+        rnn_outputs, _ = bi_rnn(BasicLSTMCell(self.hidden_size),
+                                BasicLSTMCell(self.hidden_size),
+                                inputs=batch_embedded, dtype=tf.float32)
 
-    # Accuracy metric
-    prediction = tf.argmax(tf.nn.softmax(y_hat), 1)
-    accuracy = tf.reduce_mean(tf.cast(tf.equal(prediction, tf.argmax(batch_y, 1)), tf.float32))
+        fw_outputs, bw_outputs = rnn_outputs
 
-with tf.Session(graph=graph) as sess:
+        W = tf.Variable(tf.random_normal([self.hidden_size], stddev=0.1))
+        H = fw_outputs + bw_outputs  # (batch_size, seq_len, HIDDEN_SIZE)
+        M = tf.tanh(H)  # M = tanh(H)  (batch_size, seq_len, HIDDEN_SIZE)
+
+        alpha = tf.nn.softmax(tf.matmul(tf.reshape(M, [-1, self.hidden_size]), tf.reshape(W, [-1, 1])))
+        r = tf.matmul(tf.transpose(H, [0, 2, 1]),
+                      tf.reshape(alpha, [-1, self.max_len, 1]))
+        r = tf.squeeze(r)
+        h_star = tf.tanh(r)  # (batch , HIDDEN_SIZE
+
+        h_drop = tf.nn.dropout(h_star, self.keep_prob)
+
+        # Fully connected layer（dense layer)
+        FC_W = tf.Variable(tf.truncated_normal([self.hidden_size, self.max_len], stddev=0.1))
+        FC_b = tf.Variable(tf.constant(0., shape=[self.max_len]))
+        y_hat = tf.nn.xw_plus_b(h_drop, FC_W, FC_b)
+
+        self.loss = tf.reduce_mean(
+            tf.nn.sparse_softmax_cross_entropy_with_logits(logits=y_hat, labels=self.label))
+
+        # prediction
+        self.prediction = tf.argmax(tf.nn.softmax(y_hat), 1)
+
+        # optimization
+        loss_to_minimize = self.loss
+        tvars = tf.trainable_variables()
+        gradients = tf.gradients(loss_to_minimize, tvars, aggregation_method=tf.AggregationMethod.EXPERIMENTAL_TREE)
+        grads, global_norm = tf.clip_by_global_norm(gradients, 1.0)
+
+        self.global_step = tf.Variable(0, name="global_step", trainable=False)
+        self.optimizer = tf.train.AdamOptimizer(learning_rate=self.learning_rate)
+        self.train_op = self.optimizer.apply_gradients(zip(grads, tvars), global_step=self.global_step,
+                                                       name='train_step')
+        print("graph built successfully!")
+
+    def _make_train_feed_dict(self, batch):
+        feed_dict = {self.x: batch[0],
+                     self.label: batch[1],
+                     self.keep_prob: .5}
+        return feed_dict
+
+    def _make_test_feed_dict(self, batch):
+        feed_dict = {self.x: batch[0],
+                     self.label: batch[1],
+                     self.keep_prob: 1.0}
+        return feed_dict
+
+    def run_train_step(self, sess, batch):
+        feed_dict = self._make_train_feed_dict(batch)
+        to_return = {
+            'train_op': self.train_op,
+            'loss': self.loss,
+            'global_step': self.global_step,
+        }
+        return sess.run(to_return, feed_dict)
+
+    def run_eval_step(self, sess, batch):
+        feed_dict = self._make_test_feed_dict(batch)
+        prediction = sess.run(self.prediction, feed_dict)
+        acc = np.sum(np.equal(prediction, batch[1])) / len(prediction)
+        return acc
+
+
+if __name__ == '__main__':
+    # load data
+    x_train, y_train = load_data("../dbpedia_data/dbpedia_csv/train.csv", sample_ratio=1e-2, one_hot=False)
+    x_test, y_test = load_data("../dbpedia_data/dbpedia_csv/test.csv", one_hot=False)
+
+    # data preprocessing
+    x_train, x_test, vocab_size = \
+        data_preprocessing_v2(x_train, x_test, max_len=32)
+    print("train size: ", len(x_train))
+    print("vocab size: ", vocab_size)
+
+    # split dataset to test and dev
+    x_test, x_dev, y_test, y_dev, dev_size, test_size = \
+        split_dataset(x_test, y_test, 0.1)
+    print("Validation Size: ", dev_size)
+
+    config = {
+        "max_len": 32,
+        "hidden_size": 64,
+        "vocab_size": vocab_size,
+        "embedding_size": 128,
+        "n_class": 15,
+        "learning_rate": 1e-3,
+        "batch_size": 32,
+        "train_epoch": 20
+    }
+
+    classifier = ABLSTM(config)
+    classifier.build_graph()
+
+    sess = tf.Session()
     sess.run(tf.global_variables_initializer())
-    print("Initialized! ")
-
-    print("Start trainning")
+    dev_batch = (x_dev, y_dev)
     start = time.time()
-    for e in range(epochs):
+    for e in range(config["train_epoch"]):
 
-        epoch_start = time.time()
+        t0 = time.time()
         print("Epoch %d start !" % (e + 1))
-        for x_batch, y_batch in fill_feed_dict(x_train, y_train, BATCH_SIZE):
-            fd = {batch_x: x_batch, batch_y: y_batch, keep_prob: KEEP_PROB}
-            l, _, acc = sess.run([loss, optimizer, accuracy], feed_dict=fd)
+        for x_batch, y_batch in fill_feed_dict(x_train, y_train, config["batch_size"]):
+            return_dict = classifier.run_train_step(sess, (x_batch, y_batch))
 
-        epoch_finish = time.time()
-        print("Validation accuracy and loss: ", sess.run([accuracy, loss], feed_dict={
-            batch_x: x_dev,
-            batch_y: y_dev,
-            keep_prob: 1.0
-        }))
+        t1 = time.time()
+        
+        print("Train Epoch time:  %.3f s" % (t1 - t0))
+        dev_acc = classifier.run_eval_step(sess, dev_batch)
+        print("validation accuracy: %.3f " % dev_acc)
 
     print("Training finished, time consumed : ", time.time() - start, " s")
     print("Start evaluating:  \n")
     cnt = 0
     test_acc = 0
-    for x_batch, y_batch in fill_feed_dict(x_test, y_test, BATCH_SIZE):
-            fd = {batch_x: x_batch, batch_y: y_batch, keep_prob: 1.0}
-            acc = sess.run(accuracy, feed_dict=fd)
-            test_acc += acc
-            cnt += 1        
-    
-    print("Test accuracy : %f %%" % ( test_acc / cnt * 100))
+    for x_batch, y_batch in fill_feed_dict(x_test, y_test, config["batch_size"]):
+        acc = classifier.run_eval_step(sess, (x_batch, y_batch))
+        test_acc += acc
+        cnt += 1
 
-
-
-
+    print("Test accuracy : %f %%" % (test_acc / cnt * 100))
+   
